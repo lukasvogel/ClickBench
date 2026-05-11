00dd44ff213ac76bf3966b4e618f60f237eb3d2e
Lukas Vogel <lukas@cedardb.com>
2026-05-11 14:21:53 +0200

    CedarDB with parquet and no docker

--- cedardb/benchmark.sh
+++ cedardb/benchmark.sh
@@ -1,43 +1,53 @@
 #!/bin/bash -e
-
-# docker
+export PGHOST="/tmp"
+export PGUSER=postgres
+export PGDATABASE=postgres
+export PARALLEL=48
+export RELATION_COLUMN_BLOCKSHIFT=19
+#export RELATION_COLUMN_SCANPOLICY=n
 sudo apt-get update -y
-sudo apt-get install -y docker.io postgresql-client gzip
+sudo apt-get install -y postgresql-client
 
 # download dataset
 echo "Downloading dataset..."
-sudo apt-get install -y pigz
-wget --continue --progress=dot:giga 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
-echo "Unpacking dataset..."
-pigz -d -f hits.tsv.gz
-mkdir data
-mv hits.tsv data
-chmod -R 777 data
+mkdir -p data
+cd data
+
+wget --continue --progress=dot:giga https://datasets.clickhouse.com/hits_compatible/hits.parquet
+
+cd ..
+
 rm -rf db
 mkdir db
 
 # get and configure CedarDB image
 echo "Starting CedarDB..."
-docker run --rm -p 5432:5432 -v ./data:/data -v ./db:/var/lib/cedardb/data -e CEDAR_PASSWORD=test --name cedardb cedardb/cedardb:latest > /dev/null 2>&1 &
+curl https://get.cedardb.com | bash -s -- -y
 
-# wait for container to start
-until pg_isready -h localhost --dbname postgres -U postgres > /dev/null 2>&1; do sleep 1; done
+
+# Run CedarDB in the background, store PID to kill it later
+./cedar/cedardb --createdb ./db &
+export CEDAR_PID=$!
+
+# wait for CedarDB to start
+until pg_isready > /dev/null 2>&1; do sleep 1; done
 
 # create table and ingest data
-PGPASSWORD=test  psql -h localhost -U postgres -t < create.sql 2>&1 | tee load_out.txt
+psql -f create.sql 2>&1 | tee load_out.txt
+
+echo "Inserting data..."
+echo -n "Load time: "
+
+psql -t -c "\timing" -f load.sql 2>&1 | tee load_out.txt
+
 if grep 'ERROR' load_out.txt
 then
     exit 1
 fi
-echo "Inserting data..."
-echo -n "Load time: "
-PGPASSWORD=test command time -f '%e' psql -h localhost -U postgres -q -t -c "COPY hits FROM '/data/hits.tsv';"
 
-# get ingested data size
 echo -n "Data size: "
-PGPASSWORD=test psql -h localhost -U postgres -q -t -c "SELECT pg_total_relation_size('hits');"
+psql -q -t -c "SELECT pg_total_relation_size('hits');"
 
-# run benchmark
 echo "running benchmark..."
 ./run.sh 2>&1 | tee log.txt
 
@@ -45,3 +55,9 @@ cat log.txt | \
     grep -oP 'Time: \d+\.\d+ ms|psql: error' | \
     sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/; s/^.*psql: error.*$/null/' | \
     awk '{ if (i % 3 == 0) { printf "[" }; if ($1 == "null") { printf $1 } else { printf $1 / 1000 }; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
+
+kill $CEDAR_PID
+# Wait for CedarDB to be gone
+while $(kill -0 $CEDAR_PID 2>/dev/null); do
+    sleep 1
+done
--- cedardb/run.sh
+++ cedardb/run.sh
@@ -5,20 +5,10 @@ TRIES=3
 cat queries.sql | while read -r query; do
     sync
     echo 3 | sudo tee /proc/sys/vm/drop_caches
-    docker restart $(docker ps -a -q)
-
-    retry_count=0
-    while [ $retry_count -lt 120 ]; do
-        if PGPASSWORD=test psql -h localhost -U postgres -c "SELECT 'Ok';"; then
-            break
-        fi
-
-        retry_count=$((retry_count+1))
-        sleep 1
-    done
-
+    psql -c "set debug.parallel=196;"
+    psql -c "SELECT pg_drop_caches();"
     echo "$query";
     for i in $(seq 1 $TRIES); do
-        PGPASSWORD=test psql -h localhost -U postgres -t -c '\timing' -c "$query" 2>&1 | grep -P 'Time|psql: error' | tail -n1
+        psql -t -c '\timing' -c "$query" 2>&1 | grep -P 'Time|psql: error' | tail -n1
     done
 done
